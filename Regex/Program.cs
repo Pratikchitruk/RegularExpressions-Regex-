b9d278d5b310bad7f4fd3e83585410cd0cbe0386
PratikChitruk <pratik.chitruk@gmail.com>
2022-04-13 20:32:04 +0530

    [pratik] program.cs

---
+++ Regex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UserRegistrationProblem // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+
+
+            EnterFirstName.FirstName();
+            EnterLastName.LastName();
+            Email.MyEmail();
+            MobileNo.MyNo();
+            Password.MyPassword();
+            AddUpeer.AddUpperPassword();
+            Addspacial.AddspacialPassword();
+
+
+                
+        }
+
+    }
+}
